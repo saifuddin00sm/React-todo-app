da759dac525b4f4a0a83cc3b3bb86f6c7bd2f098
Saif Uddin <shyboy.skk@gmail.com>
2021-07-15 02:55:34 +0600

    small-changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import mainStyles from "./App.module.css";
-import Task from "./InputTask/InputTask";
+import Task from "./Components/InputTask/InputTask";
 
 class TodoApp extends Component {
   state = {
@@ -16,9 +16,54 @@ class TodoApp extends Component {
     } else {
       const myArr = this.state.tasks;
       myArr.push(input.value);
+
       this.setState({ tasks: myArr });
+
+      const taskVal = input.value.trim();
+
+      this.setItemOnLocalHandler(taskVal);
       input.value = "";
     }
+  };
+
+  // Setting task to the local storage
+  setItemOnLocalHandler = (tasks) => {
+    let task;
+    if (localStorage.getItem("tasks") == null) {
+      task = [];
+    } else {
+      task = JSON.parse(localStorage.getItem("tasks"));
+    }
+    task.push(tasks);
+    localStorage.setItem("tasks", JSON.stringify(task));
+  };
+
+  //getting local storage items
+  getItemFromLocalHandler = () => {
+    let task;
+
+    if (localStorage.getItem("tasks") == null) {
+      task = [];
+    } else {
+      task = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    this.setState({ tasks: task });
+  };
+
+  // remove task from localStorage
+  removeTaskfromLocalHandler = (ind) => {
+    let task;
+
+    if (localStorage.getItem("tasks") == null) {
+      task = [];
+    } else {
+      task = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    task.splice(ind, 1);
+    localStorage.setItem("tasks", JSON.stringify(task));
+    this.setState({ tasks: task });
   };
 
   // Removing task from list
@@ -26,7 +71,14 @@ class TodoApp extends Component {
     const tasks = this.state.tasks;
     tasks.splice(index, 1);
     this.setState({ tasks: tasks });
+
+    this.removeTaskfromLocalHandler(index);
   };
+
+  // document on load getting localStorage items
+  componentDidMount() {
+    window.addEventListener("load", this.getItemFromLocalHandler);
+  }
 
   render() {
     const taskList = this.state.tasks.map((t, index) => {
