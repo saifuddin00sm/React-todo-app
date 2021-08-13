80592b0d8e1b6da63eb9a3ebd0379bd6ab68c3b9
Saif Uddin <shyboy.skk@gmail.com>
2021-08-13 18:01:30 +0600

    minal changes

--- src/Components/InputTask/InputTask.js
+++ src/Components/InputTask/InputTask.js
@@ -1,10 +1,11 @@
 import React from "react";
 import inputClassses from "./InputTask.module.css";
 
-const tasks = (props) => {
+const tasks = props => {
   return (
     <li className={inputClassses.listStyle}>
       <span>{props.items}</span>
+      {/* <span>Date and times</span> */}
       <span onClick={props.remove} className={inputClassses.closeBtn}>
         &#10005;
       </span>
